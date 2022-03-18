31361a97368003dd2f506f671ce7ecebe911f308
Marco Crisostomi <m.crisostomi91@gmail.com>
2022-03-18 19:11:51 +0100

    vue.js to do list

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 const app = new Vue({
     el: '#root',
     data: {
-
+        newElement: '',
+        thingsToDo: []
     },
     methods: {
-
+        addListElement (element) {
+            if (element.trim() != '') {
+                this.thingsToDo.push(element);
+                this.newElement = '';
+                console.log('elemento aggiunto');
+            } else {
+                console.log('non hai aggiunto nessun elemento')
+            }
+        },
+        removeListElement (elementIndex) {
+            this.thingsToDo.splice(elementIndex, 1)
+            console.log('elemento rimosso');
+        }
     }
 });
